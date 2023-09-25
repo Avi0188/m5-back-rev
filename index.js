f0b76926fbfbf94a3a864744ac6c4cec0affacb7
unknown <rai2889@gmail.com>
2023-09-25 18:24:40 +0530

    cors

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const connection=require("./config/db")
 const userRouter = require("./routes/user.route")
 const employeeRouter = require("./routes/employee.route")
 const app=express()
-
+const cors=require("cors")
 
 app.use(express.json())
+app.use(cors())
 app.use("/users",userRouter)
 app.use("/employees",employeeRouter)
 app.listen(8000,async()=>{
